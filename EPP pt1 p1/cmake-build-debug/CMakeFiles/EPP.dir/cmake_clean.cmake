3cc51ea90d4008aa9c5ee2ea99c387206c0ab43e
ChristopherMillones <53508840+ChristopherMillones@users.noreply.github.com>
2019-11-19 17:20:35 -0800

    Add files via upload

---
+++ EPP pt1 p1/cmake-build-debug/CMakeFiles/EPP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EPP.dir/main.cpp.obj"
+  "CMakeFiles/EPP.dir/IntegerList.cpp.obj"
+  "EPP.pdb"
+  "EPP.exe"
+  "EPP.exe.manifest"
+  "libEPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
