7134cee1535c702051f6eaf3eaf4103cb48e2d4f
Christopher <ChrisMillonesl1@hotmail.com>
2019-12-23 11:22:45 -0800

    EPP pt1 p1 Complete

---
+++ EPP pt2/cmake-build-debug/CMakeFiles/EPP_pt2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EPP_pt2.dir/main.cpp.obj"
+  "EPP_pt2.pdb"
+  "EPP_pt2.exe"
+  "EPP_pt2.exe.manifest"
+  "libEPP_pt2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EPP_pt2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EPP pt2/cmake-build-debug/CMakeFiles/EPP_pt2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EPP_pt2.
+# This may be replaced when dependencies are built.
